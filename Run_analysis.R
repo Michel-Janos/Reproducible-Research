c625e19d8891d32b84cbae99407f5102558e954d
Michel-Janos <michel@3dimensoes.com.br>
2015-06-05 13:39:55 -0300

    version 1

--- Run_analysis.R
+++ Run_analysis.R
@@ -16,10 +16,13 @@ X_test <- read.table("X_test.txt", header = FALSE)
 ##################################################################
 ds_xtr_xte <- rbind(X_train,X_test)
 
+
+
 #joining headers("features")
 YYY <- read.table("features.txt" , header=F, sep="")
 zzz <- YYY[, -c(1)] # excluding 1st column
 www <- t(zzz) # transposing matrix
+
 
 head(www)
 
@@ -53,13 +56,22 @@ str(ds_xtr_xte)
 # 4 - joining y datasets
 ##################################################################
 
-
-
 activ <- rbind(y_train,y_test)
-ds_activ <- cbind(activ,ds_xtr_xte ) # attaching column to x dataset
-names(ds_activ)[names(ds_activ)=="V1"] <- "activity" # changing names of column
 
 
+##################################################################
+#  5 change  activities names and headers
+##################################################################
+class(activ)
+as.factor(activ$V1)
+
+
+library(plyr)
+
+new_act_names <- mapvalues(activ$V1, from = c("1","2","3","4","5","6"), to = c("walking", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
+
+ds_activ <- cbind(new_act_names,ds_xtr_xte ) # attaching column to x dataset
+names(ds_activ)[names(ds_activ)=="new_act_names"] <- "activity" # changing names of column
 
 person <- rbind(subject_train,subject_test)
 ds_person <- cbind(person,ds_activ)  # attaching column of subjects to ds_activ dataset
@@ -73,15 +85,14 @@ str(ds_complete)
 
 # 'data.frame':  10299 obs. of  563 variables:
 #   $ person                              : int  1 1 1 1 1 1 1 1 1 1 ...
-# $ activity                            : int  5 5 5 5 5 5 5 5 5 5 ...
+# $ activity                            : Factor w/ 6 levels "LAYING","SITTING",..: 3 3 3 3 3 3 3 3 3 3 ...
 # $ tBodyAcc-mean()-X                   : num  0.289 0.278 0.28 0.279 0.277 ...
 # $ tBodyAcc-mean()-Y                   : num  -0.0203 -0.0164 -0.0195 -0.0262 -0.0166 ...
 # $ tBodyAcc-mean()-Z                   : num  -0.133 -0.124 -0.113 -0.123 -0.115 ...
-# $ tBodyAcc-std()-X                    : num  -0.995 -0.998 -0.995 -0.996 -0.998 ...
-# $ tBodyAcc-std()-Y                    : num  -0.983 -0.975 -0.967 -0.983 -0.981 ...
+# $ tBodyAcc-std()-X                    : num  -0.995 -0.998 -0.995 -0.996 -0.998 ... ...
 
 ##################################################################
-# 5 - selecting columns (means, sd)
+# 6 - selecting columns (means, sd)
 ##################################################################
 complete_final <- ds_complete[, c(1:8,43:48,83:88,123:128,163:168, 203:204,229:230,242:243,255:256,268,273,296:298,347:352,426:431,505:506,515,516:519,528,531:532,544:545,554,557:563 )]
 
@@ -103,64 +114,74 @@ str(complete_final)
 # $ tGravityAcc-mean()-Z                : num  0.1154 0.1094 0.1019 0.0999 0.0945 ...
 
 
-##################################################################
-# 6 -  From complete_final , creates a second, independent tidy data set 
-#with the average of each variable for each activity and each subject.
-##################################################################
 
 
 
-tidy <- aggregate(. ~ person+activity, data = complete_final, mean)
+
+#tidy <- aggregate(. ~ person+activity, data = complete_final, mean)
 
 ##################################################################
 # 7 - change labels of activities 
 ##################################################################
 
 
-atri_factors<- read.table("act_labels.txt" , header = FALSE)
-
-tidy <-cbind(atri_factors, tidy)
+#names(complete_final)[names(complete_final)=="activityLabels$V2"] <- "Activ"
 
 
-names(tidy)[names(tidy)=="V1"] <- "Activity"
+names(complete_final)<-gsub("^t", "time", names(complete_final))
+names(complete_final)<-gsub("^f", "frequency", names(complete_final))
+names(complete_final)<-gsub("Acc", "Accelerometer", names(complete_final))
+names(complete_final)<-gsub("Gyro", "Gyroscope", names(complete_final))
+names(complete_final)<-gsub("Mag", "Magnitude", names(complete_final))
+names(complete_final)<-gsub("BodyBody", "Body", names(complete_final))
+
+names(complete_final)
+
+# [1] "person"                                             "activity"                                          
+# [3] "timeBodyAccelerometer-mean()-X"                     "timeBodyAccelerometer-mean()-Y"                    
+# [5] "timeBodyAccelerometer-mean()-Z"                     "timeBodyAccelerometer-std()-X"                     
+# [7] "timeBodyAccelerometer-std()-Y"                      "timeBodyAccelerometer-std()-Z"                     
+# [9] "timeGravityAccelerometer-mean()-X"                  "timeGravityAccelerometer-mean()-Y"                 
+# [11] "timeGravityAccelerometer-mean()-Z"                  "timeGravityAccelerometer-std()-X"                  
+# [13] "timeGravityAccelerometer-std()-Y"                   "timeGravityAccelerometer-std()-Z"  
+
 
 ##################################################################
-# 8 - Delete column "activity"
+# 8 -  From complete_final , creates a second, independent tidy data set 
+#with the average of each variable for each activity and each subject.
 ##################################################################
 
-tidy <- tidy[, -c(3)]
+
+tidy <- aggregate(. ~ person+activity, data = complete_final, mean)
 
 head(tidy)
 
-# Activity person tBodyAcc-mean()-X tBodyAcc-mean()-Y tBodyAcc-mean()-Z tBodyAcc-std()-X tBodyAcc-std()-Y tBodyAcc-std()-Z tGravityAcc-mean()-X
-# 1  WALKING      1         0.2773308       -0.01738382        -0.1111481       -0.2837403       0.11446134       -0.2600279            0.9352232
-# 2  WALKING      2         0.2764266       -0.01859492        -0.1055004       -0.4236428      -0.07809125       -0.4252575            0.9130173
-# 3  WALKING      3         0.2755675       -0.01717678        -0.1126749       -0.3603567      -0.06991407       -0.3874120            0.9365067
-# 4  WALKING      4         0.2785820       -0.01483995        -0.1114031       -0.4408300      -0.07882674       -0.5862528            0.9639997
-# 5  WALKING      5         0.2778423       -0.01728503        -0.1077418       -0.2940985       0.07674840       -0.4570214            0.9726250
-# 6  WALKING      6         0.2836589       -0.01689542        -0.1103032       -0.2965387       0.16421388       -0.5043242            0.9580675
-
-tail(tidy)
-
-# Activity person tBodyAcc-mean()-X tBodyAcc-mean()-Y tBodyAcc-mean()-Z tBodyAcc-std()-X tBodyAcc-std()-Y tBodyAcc-std()-Z tGravityAcc-mean()-X
-# 175   LAYING     25         0.2507918       -0.01889437        -0.1004288       -0.9091165       -0.6917707       -0.7172620           -0.4611025
-# 176   LAYING     26         0.2716459       -0.01918957        -0.1050025       -0.9694454       -0.9832314       -0.9845000           -0.6213097
-# 177   LAYING     27         0.2741025       -0.01798676        -0.1076997       -0.9784552       -0.9837360       -0.9866370           -0.5304346
-# 178   LAYING     28         0.2759135       -0.01675379        -0.1083449       -0.9688883       -0.9453868       -0.9564503           -0.4903345
-# 179   LAYING     29         0.2872952       -0.01719655        -0.1094621       -0.9842196       -0.9902409       -0.9872551           -0.3467898
-# 180   LAYING     30         0.2810339       -0.01944941        -0.1036582       -0.9763625       -0.9542018       -0.9670442           -0.3447378
+# person activity timeBodyAccelerometer-mean()-X timeBodyAccelerometer-mean()-Y timeBodyAccelerometer-mean()-Z timeBodyAccelerometer-std()-X
+# 1      1   LAYING                      0.2215982                    -0.04051395                     -0.1132036                    -0.9280565
+# 2      2   LAYING                      0.2813734                    -0.01815874                     -0.1072456                    -0.9740595
+# 3      3   LAYING                      0.2755169                    -0.01895568                     -0.1013005                    -0.9827766
+# 4      4   LAYING                      0.2635592                    -0.01500318                     -0.1106882                    -0.9541937
+# 5      5   LAYING                      0.2783343                    -0.01830421                     -0.1079376                    -0.9659345
+# 6      6   LAYING                      0.2486565                    -0.01025292                     -0.1331196                    -0.9340494
 
 
+# checking the dataset
+table(tidy[, c("activity", "person")])
 
-
-
-table(tidy[, c("Activity", "person")])
-
-
+person
+activity             1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+LAYING             1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+SITTING            1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+STANDING           1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+walking            1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+WALKING_DOWNSTAIRS 1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
+WALKING_UPSTAIRS   1 1 1 1 1 1 1 1 1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1  1
 ##################################################################
 # 9 - saving tidy.txt in wd
 ##################################################################
 write.table(tidy, file = "tidy.txt", row.names = FALSE)
+
+
 
 
 
